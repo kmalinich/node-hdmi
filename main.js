56f3c54530c9ed4961f5994e8e1bf17bb5e04f82
kdm <kennygprs@gmail.com>
2018-01-03 16:33:56 -0500

    Update

--- main.js
+++ main.js
@@ -1,67 +1,56 @@
-let module_name = __filename.slice(__dirname.length + 1, -3);
+/* eslint no-console : 0 */
+
 app_path = __dirname;
 
 // Global libraries
+api    = require('api');
+hdmi   = require('hdmi');
 json   = require('json');
 log    = require('log-output');
-os     = require('os');
 update = require('update');
 
-// WebSockets
-// socket_client = require('socket-client');
-// socket_server = require('socket-server');
 
-// API config - should be moved into API object
-dispatcher          = new (require('httpdispatcher'))();
-http                = require('http');
-query_string        = require('querystring');
-api_socket_key_last = 0;
-api_socket_map      = {};
-api_server          = http.createServer(api_handler);
-api_header          = {
-	'Content-Type'  : 'application/json',
-	'Cache-Control' : 'no-cache',
-};
+// Configure term event listeners
+function term_config(pass) {
+	process.on('SIGTERM', () => {
+		console.log('');
+		log.msg({ msg : 'Caught SIGTERM' });
+		process.nextTick(term);
+	});
 
-// Load HDMI library
-function hdmi_load(hdmi_load_callback = null) {
-	HDMI = require('HDMI');
+	process.on('SIGINT', () => {
+		console.log('');
+		log.msg({ msg : 'Caught SIGINT' });
+		process.nextTick(term);
+	});
 
-	typeof hdmi_load_callback === 'function' && hdmi_load_callback();
-	hdmi_load_callback = undefined;
+	process.on('exit', () => {
+		log.msg({ msg : 'Terminated' });
+	});
+
+	process.nextTick(pass);
 }
 
-// Global startup
-function startup() {
-	log.msg({
-		src : module_name,
-		msg : 'Starting',
-	});
+// Global init
+function init() {
+	log.msg({ msg : 'Initializing' });
 
 	json.read(() => { // Read JSON config and status files
-		api_startup(() => { // Open API server
-			hdmi_load(() => { // Load HDMI library
-				HDMI.init(() => { // Open HDMI-CEC
-					log.msg({
-						src : module_name,
-						msg : 'Started',
-					});
-				});
-			});
-		});
-	});
+		api.init(() => { // Start API server(s)
+			hdmi.init(() => { // Start HDMI-CEC
+				log.msg({ msg : 'Initialized' });
+			}, term);
+		}, term);
+	}, term);
 }
 
 // Global shutdown
-function shutdown() {
-	log.msg({
-		src : module_name,
-		msg : 'Shutting down',
-	});
+function term() {
+	log.msg({ msg : 'Terminating' });
 
 	json.write(() => { // Write JSON config and status files
-		HDMI.term(() => { // Close HDMI-CEC
-			api_shutdown(() => { // Close API server
+		api.term(() => { // Stop API server(s)
+			hdmi.term(() => { // Stop HDMI-CEC
 				process.exit();
 			});
 		});
@@ -69,124 +58,5 @@ function shutdown() {
 }
 
 
-// Port 3001 listener for POST requests to modules
-// This REALLY REALLY REALLY REALLY should be moved into it's own object
-
-function api_startup(api_startup_callback = null) {
-	// error handling breh
-	api_server.listen(config.server.port, () => {
-		log.msg({
-			src : 'API',
-			msg : 'API server up, port ' + config.server.port,
-		});
-
-		typeof api_startup_callback === 'function' && api_startup_callback();
-		api_startup_callback = undefined;
-
-		api_server.on('connection', (api_socket) => {
-			// Generate a new, unique api_socket-key
-			let api_socket_key = ++api_socket_key_last;
-
-			// Add api_socket when it is connected
-			api_socket_map[api_socket_key] = api_socket;
-
-			// Remove api_socket when it is closed
-			api_socket.on('close', () => {
-				delete api_socket_map[api_socket_key];
-			});
-		});
-	});
-}
-
-// Close API server and kill the sockets
-function api_shutdown(api_shutdown_callback = null) {
-	// Loop through all sockets and destroy them
-	Object.keys(api_socket_map).forEach((api_socket_key) => {
-		api_socket_map[api_socket_key].destroy();
-	});
-
-	// Tell server to close
-	api_server.close();
-
-	// API server close event
-	api_server.on('close', () => {
-		log.msg({
-			src : 'API',
-			msg : 'Stopped',
-		});
-
-		typeof api_shutdown_callback === 'function' && api_shutdown_callback();
-		api_shutdown_callback = undefined;
-	});
-}
-
-// API handler function
-function api_handler(request, response) {
-	dispatcher.dispatch(request, response);
-}
-
-// HDMI POST request
-dispatcher.onPost('/hdmi', (request, response) => {
-	HDMI.command(query_string.parse(request.body).command, response);
-});
-
-dispatcher.onGet('/hdmi', (request, response) => {
-	response.writeHead(200, api_header);
-	response.end(JSON.stringify(status.hdmi));
-});
-
-dispatcher.onGet('/config', (request, response) => {
-	response.writeHead(200, api_header);
-	response.end(JSON.stringify(config));
-});
-
-dispatcher.onGet('/status', (request, response) => {
-	response.writeHead(200, api_header);
-	response.end(JSON.stringify(status));
-});
-
-// Error
-dispatcher.onError((request, response) => {
-	log.msg({
-		src : module_name,
-		msg : 'API: 404',
-	});
-
-	response.writeHead(404);
-	response.end();
-});
-
-
-// Shutdown events/signals
-process.on('SIGTERM', () => {
-	log.msg({
-		src : module_name,
-		msg : 'Received SIGTERM, launching shutdown()',
-	});
-	shutdown();
-});
-
-process.on('SIGINT', () => {
-	log.msg({
-		src : module_name,
-		msg : 'Received SIGINT, launching shutdown()',
-	});
-	shutdown();
-});
-
-process.on('SIGPIPE', () => {
-	log.msg({
-		src : module_name,
-		msg : 'Received SIGPIPE, launching shutdown()',
-	});
-	shutdown();
-});
-
-process.on('exit', () => {
-	log.msg({
-		src : module_name,
-		msg : 'Shut down',
-	});
-});
-
-startup();
+// FASTEN SEATBELTS
+term_config(init);
